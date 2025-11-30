3da4e5517deb16d74d5e423980d5cd4a8763a4d0
Фнилер <leshag999@gmail.com>
2025-11-30 22:44:41 +0300

    Создал общую структуру

---
+++ YPPcommand/education.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YPPcommand
+{
+    internal class education
+    {
+        enum Education
+        {
+            Specialist,
+            Вachelor,
+            SecondEducation,
+        }
+    }
+}
---
+++ YPPcommand/exam.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YPPcommand
+{
+    internal class exam
+    {
+        class Exam
+        {
+            public string Namepredmet { get; set; }
+            public int Otsenka { get; set; }
+            public DateTime Data { get; set; }
+
+            public Exam(string Namepredmet, int Otsenka, DateTime Data)
+            {
+                this.Namepredmet = Namepredmet;
+                this.Otsenka = Otsenka;
+                this.Data = Data;
+            }
+            public Exam()
+            {
+                Namepredmet = "kpiap";
+                Otsenka = 10;
+                Data = new DateTime(2025, 2, 4);
+            }
+            public override string ToString()
+            {
+                return $"Предмет: {Namepredmet} Оценка: {Otsenka} дата: {Data}";
+            }
+        }
+    }
+}
