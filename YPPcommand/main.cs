2ea2f89172fa110bbdc3a015c990caec9139d645
Yorih0 <catqwe946@gmail.com>
2025-12-01 09:27:10 +0300

    main 0.5

--- YPPcommand/main.cs
+++ YPPcommand/main.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.Common;
+using YPPcommand;
+
+Main();
+static void Main()
+{
+    Console.WriteLine("Введите размерность массива Col - , Row - ");
+    string input = Console.ReadLine();
+    char[] separators = { ' ', ',', ';', '\t' };
+    string[] mass = input.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+    int nrow = int.Parse(mass[0]);
+    int ncol = int.Parse(mass[1]);
+    Person[] MD1 = new Person[nrow * ncol];
+    for(int i = 0;i<MD1.Length;i++)
+    {
+        MD1.SetValue(new Person(),i); 
+    }
+    Person[,] MD2 = new Person[nrow, ncol];
+    for (int i = 0; i < MD2.GetLength(0); i++)
+    {
+        for (int j = 0; j < MD2.GetLength(1); j++)
+        {
+            
+            MD2.SetValue(new Person(),i,j);
+        }
+    }
+    Person[][] MD2_j = new Person[nrow][];
+    for (int i = 0; i < MD2_j.GetLength(0); i++)
+    {
+        MD2_j.SetValue(new Person[ncol],i);
+        for (int j = 0; j < ncol; j++)
+        {
+            MD2.SetValue(new Person(),i,j);
+        }
+    }
+    int startTime = Environment.TickCount;
+    foreach(var item in MD1)
+    {
+
+    }
+}
