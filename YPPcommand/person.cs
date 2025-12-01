54224d38a4425db811a0db5ae95ef3e2223261c9
Yorih0 <catqwe946@gmail.com>
2025-12-01 08:44:52 +0300

    person

--- YPPcommand/person.cs
+++ YPPcommand/person.cs
@@ -4,7 +4,87 @@ using System.Text;
 
 namespace YPPcommand
 {
-    internal class Person
+    class Person
     {
+        protected string name;
+        protected string last_name;
+        protected DateTime birthday;
+
+        public Person(string name, string last_name, DateTime birthday)
+        {
+            this.name = name;
+            this.last_name = last_name;
+            this.birthday = birthday;
+        }
+        public Person() : this("неизвестно", "неизвестно", DateTime.Now) { }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value != null)
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new Exception("Нет значения");
+                }
+            }
+        }
+        public string Last_Name
+        {
+            get { return last_name; }
+            set
+            {
+                if (value != null)
+                {
+                    last_name = value;
+                }
+                else
+                {
+                    throw new Exception("Нет значения");
+                }
+            }
+        }
+        public DateTime Birthday
+        {
+            get { return birthday; }
+            set
+            {
+                if (value.Year<120)
+                {
+                    birthday = value;
+                }
+                else
+                {
+                    throw new Exception("Неверное значение");
+                }
+            }
+        }
+        public int Year
+        {
+            get { return birthday.Year; }
+            set
+            {
+                if(value > 0 )
+                {
+                    birthday = birthday.AddYears(value);
+                }
+                else 
+                {
+                    throw new Exception("Неверное значение");
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"Name:{name}|Last name:{last_name}|Birthday:{birthday}";
+        }
+        public virtual  string ToShortString()
+        {
+            return $"Name:{name}|Last name:{last_name}";
+        }
     }
 }
